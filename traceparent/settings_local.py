3e2fbf1d9d6f6ad1402556fe84f70c7364f4923d
Bert Poller <bpoller@ekito.fr>
2013-03-19 11:38:00 +0100

    fixed some stuff

---
+++ traceparent/settings_local.py
@@ -0,0 +1,18 @@
+ADMINS = (
+     ('Bert Poller', 'bpoller@ekito.fr'),
+)
+DEFAULT_FROM_EMAIL = ADMINS[0][1]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'traceparent.sqlite',                      # Or path to database file if using sqlite3.
+        #'USER': 'traceparent',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+       # 'STORAGE_ENGINE': 'MyISAM', # For MySQL.
+    }
+}
+
+SECRET_KEY = ''
